68006e4a631f63973ebff78454e940e7a0f9dd93
gajapathy-s <151958474+gajapathy-s@users.noreply.github.com>
2024-07-06 13:25:12 +0530

    Delete catkin_ws directory

--- catkin_ws/build/computer_vision_basics/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/computer_vision/catkin_ws/src/computer_vision_basics
-# Build directory: /home/user/computer_vision/catkin_ws/build/computer_vision_basics
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
